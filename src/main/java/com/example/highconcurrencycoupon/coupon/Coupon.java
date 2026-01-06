ace0caed1eb99206e62a52cd24175596d3adf28d
JH <jiho4890@gmail.com>
2026-01-06 23:54:35 +0900

    feat: Coupon 엔티티 추가
    쿠폰의 정보를 담는 핵심 모델

---
+++ src/main/java/com/example/highconcurrencycoupon/coupon/Coupon.java
@@ -0,0 +1,39 @@
+package com.example.highconcurrencycoupon.coupon;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title; // 쿠폰 이름
+
+    @Column(nullable = false)
+    private int totalQuantity; // 총 발급 수량
+
+    @Column(nullable = false)
+    private int issuedQuantity; // 현재까지 발급된 수량
+
+    public Coupon(String title, int totalQuantity) {
+        this.title = title;
+        this.totalQuantity = totalQuantity;
+        this.issuedQuantity = 0;
+    }
+
+    // 쿠폰 발급 가능 여부 확인 및 수량 증가
+    public void issue() {
+        if (this.issuedQuantity >= this.totalQuantity) {
+            throw new RuntimeException("모든 쿠폰이 소진되었습니다.");
+        }
+        this.issuedQuantity++;
+    }
+}
